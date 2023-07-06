0180e2db12d2cdf5015408a77bca6684c5822f5e
manhnv56 <manhnv_56@vnu.edu.vn>
2023-07-07 06:47:25 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 if [ -z "$HTTP_PORT" ]; then
     # set default port
-    export HTTP_PORT=3130
+    export HTTP_PORT=3131
 fi
 
 if [ -z "$SOCKS_PORT" ]; then
     # set default port
-    export SOCKS_PORT=1081
+    export SOCKS_PORT=1082
 fi
 
 # copy config template and replace variables (only first run)
